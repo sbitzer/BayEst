cd2ba76d230183e0923e0e8f623ca2727640c24a
Sebastian Bitzer <sebastian.bitzer@tu-dresden.de>
2018-09-19 16:12:07 +0200

    more thorough investigation of decision variable dynamics
    These are changes from some time ago. I don't fully recall what I did and found
    out. I vaguely remember that I started this to investigate why the fitted model
    predicts a considerably larger difference between easy and hard accuracies than
    I found in the data.  I found out that the decision variable dynamics is
    nonlinear with initially fast rise which then drops to a more moderate and
    consistent rise. A plot of the estimated posterior probability over directions
    suggests that inference works well, but that the initial fast rise of the
    decision variable is accompanied by an initially broader posterior over
    directions.

--- simulated_example.py
+++ simulated_example.py
@@ -6,29 +6,37 @@ Created on Fri Apr 13 15:01:37 2018
 @author: bitzer
 """
 
-import rotated_directions as rtmodels
 import numpy as np
 import matplotlib.pyplot as plt
 
+import helpers
+from rotated_directions import rotated_directions as dirmodel
+
+
+#%% create model based on experimental data
+data = helpers.load_subject(19, exclude_to=False, censor_late=True)
+N = data.shape[0]
+
+choices = [1, -1]
+ndtdist = 'uniform'
+model = dirmodel(
+        data.tarDir / 180. * np.pi, helpers.dt, 
+        data.tarDir.unique() / 180. * np.pi, data.critDir / 180. * np.pi, 
+        maxrt=helpers.maxrt + helpers.dt, toresponse=helpers.toresponse, 
+        choices=choices, ndtdist=ndtdist)
+
+model.bound = 0.7
+model.intstd = 0.18
+model.noisestd = 1e-7
+
+
+#%% plot example model dynamics
+seed = 48291043
+fig, ax = model.plot_example_timecourses(
+        np.arange(20), dvtype='logprobdiff', seed=seed)
+
 
 #%% create and simulate from model
-C = 6
-D = 12
-N = 1000
-
-directions = np.linspace(0, 2 * np.pi - 2 * np.pi / D, D)
-Trials = directions[np.random.randint(D, size=N)]
-criteria = np.linspace(0, np.pi - np.pi / C, C)
-criteria = criteria[np.random.randint(C, size=N)]
-
-dt = 0.05
-
-model = rtmodels.rotated_directions(Trials, dt, directions, criteria, maxrt=2)
-
-model.noisestd = 1
-model.intstd = 0.7
-model.bound = 0.51
-model.lapseprob = 0
 choices, rts = model.gen_response(np.arange(N))
 
 plt.figure()
@@ -37,14 +45,12 @@ model.plot_response_distribution(choices, rts)
 is_correct = choices == model.correct
 is_correct.mean()
 
-fig, ax = model.plot_example_timecourses(np.arange(min(N, 200)))
-
 
 #%% log-probability increments increase as more evidence is sampled
 times, logprob_cw, logprob, logpost, logliks = model.gen_timecourses(
         np.arange(N))
 
-fig, axes = plt.subplots(1, 3, sharex=True, figsize=(10, 4))
+fig, axes = plt.subplots(1, 4, sharex=True, figsize=(12, 4))
 
 axes[0].plot(times[1:], np.abs(logliks[:, 0, :]).mean(axis=1), label='loglik')
 axes[0].set_title('|log-lik|')
@@ -57,7 +63,27 @@ diffs = np.diff(logprob_cw, axis=0)
 axes[2].plot(times[1:], np.abs(diffs).mean(axis=1), label='logp-diff')
 axes[2].set_title('|log prob diff| (cw)')
 
+diffs = np.diff(logprob_cw - np.log(1 - np.exp(logprob_cw)), axis=0)
+axes[3].plot(times[1:], np.abs(diffs).mean(axis=1), label='logp-diff')
+axes[3].set_title('|LLR diff|')
+
 for ax in axes:
     ax.set_xlabel('time (s)')
 
 fig.tight_layout()
+
+
+#%% check posterior probability densities/distribs over directions across time
+P = 200
+cols = plt.cm.Blues(np.linspace(0, 1, P))
+
+# directions are unsorted in model so find order for displaying densities
+sortind = np.argsort(model.directions)
+
+fig, ax = plt.subplots()
+
+for lp, col, p in zip(logprob[:P, :, 0], cols, range(P)):
+    ax.plot(np.exp(lp[sortind]), color=col, label='t={}'.format(p * model.dt))
+#    ax.plot(lp[sortind], color=col, label='t={}'.format(p * model.dt))
+    
+#ax.legend()
