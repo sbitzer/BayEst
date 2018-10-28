03abca58a3be019c653451c7143fa7ac4545f25a
Sebastian Bitzer <official@sbitzer.eu>
2018-10-28 23:17:00 +0100

    also creating necessary folder structure + add mkl-service for theano

--- create_snl_environment.sh
+++ create_snl_environment.sh
@@ -6,7 +6,19 @@ source ~/.bashrc
 
 SNL_NAME=snl_t2
 
-conda create -c conda-forge -n $SNL_NAME python=2.7 mkl=2017 theano ipython scipy pandas pytables numba seaborn
+# where is the behavioural data on this machine?
+#DATA_DIR_PREFIX="/dune/Experiments/DR-BayAtt-2"
+DATA_DIR_PREFIX="/home/bitzer/ZIH/projects/DraganRangelov/data/behaviour"
+
+# setting up data and results directories as needed in python code
+mkdir -p data
+ln -s $DATA_DIR_PREFIX data/behaviour
+
+mkdir -p inf_results/behaviour/snl/rotated_directions
+
+conda create -c conda-forge -n $SNL_NAME python=2.7 mkl=2017 mkl-service \
+    theano ipython scipy pandas pytables numba seaborn
+
 source activate $SNL_NAME
 
 git clone git@github.com:sbitzer/BayEst.git
