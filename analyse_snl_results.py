b70027378af9140648bc523d379ead56dfb070bf
Sebastian Bitzer <official@sbitzer.eu>
2019-03-03 17:24:20 +0100

    first proper comparison between results from old and new model
    The changes and analyses I made here were from November 2019 and I don't
    remember all the details (e.g., why did I exclude subject 24?). Anyway,
    the results made me stop working on this until now, because they showed
    that the new diff model fits the data clearly worse than the old simple
    model, when you directly compare the posterior predictive summary
    statistics. My best guess is that this has to do with bad inference, but
    it's unclear whether this is the case.

--- analyse_snl_results.py
+++ analyse_snl_results.py
@@ -15,19 +15,21 @@ import helpers
 import snl_simulators as snlsim
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 #%% 
 stats = 'hist'
 
 result = os.path.join(helpers.resultsdir, 
-                      'snl/rotated_directions/201810090117')
+                      'snl/rotated_directions/201811021934')
 
 comparison_result = os.path.join(
         helpers.resultsdir, 'snl/rotated_directions/201807061824')
 
 subjects = [int(os.path.basename(f)[1:3]) 
             for f in glob(os.path.join(result, '*%s.log' % stats))]
+subjects = sorted(subjects)[:-1]
 
 
 #%%
@@ -39,30 +41,47 @@ with pd.HDFStore(os.path.join(
     S = psamples.loc[R].shape[0]
 
 
-#%%
+#%% load results and prepare measures
 diff_data = pd.DataFrame([], columns=['accuracy', 'median RT'], 
-                         index=subjects)
+                         index=subjects, dtype=float)
 diff_post = pd.DataFrame([], columns=['accuracy', 'median RT'],
                          index = pd.MultiIndex.from_product(
                                  [subjects, np.arange(S)],
-                                 names=['subject', 'sample']))
+                                 names=['subject', 'sample']), dtype=float)
+diff_post_comp = pd.DataFrame(
+        [], columns=['accuracy', 'median RT'],
+        index = pd.MultiIndex.from_product(
+                [subjects, np.arange(S)], names=['subject', 'sample']), 
+        dtype=float)
     
+loglik = []
+loglik_comp = []
 for sub in subjects:
     print('\rProcessing subject %2d ...' % sub)
     
-    #%% generate posterior predictive data
+    with pd.HDFStore(os.path.join(
+            result, 's{:02d}_{}.h5'.format(sub, stats)), 'r') as store:
+        loglik.append(store['logdens']['loglik'].loc[R])
+    
+    # generate posterior predictive data
     choices_post, rts_post, data, model = (
             snlsim.generate_posterior_predictive_data(result, sub, stats))
     
-    if len(subjects) == 1 and comparison_result:
+    # check fit to differences in median RT and accuracy across difficulties
+    diff_post.loc[sub, 'accuracy'], diff_post.loc[sub, 'median RT'] = (
+            helpers.diff_diff(choices_post, rts_post, data.easy, 
+                              model.correct))
+    
+    if comparison_result:
+        with pd.HDFStore(os.path.join(
+                comparison_result, 's{:02d}_{}.h5'.format(sub, stats)), 'r') as store:
+            loglik_comp.append(store['logdens']['loglik'].loc[R])
+        
         choices_post_comp, rts_post_comp, data_comp, model_comp = (
             snlsim.generate_posterior_predictive_data(
                     comparison_result, sub, stats))
-    
-    
-    #%% check fit to differences in median RT and accuracy across difficulties
-    diff_post.loc[sub, 'accuracy'], diff_post.loc[sub, 'median RT'] = (
-            helpers.diff_diff(choices_post, rts_post, data.easy, 
+        diff_post_comp.loc[sub, 'accuracy'], diff_post_comp.loc[sub, 'median RT'] = (
+            helpers.diff_diff(choices_post_comp, rts_post_comp, data.easy, 
                               model.correct))
     
     dacc, dmed = helpers.diff_diff(
@@ -70,6 +89,66 @@ for sub in subjects:
     diff_data.loc[sub] = np.r_[dacc, dmed]
     
 print('done.')
+
+loglik = pd.concat(loglik, axis=1)
+loglik.columns = pd.Index(subjects, name='subject')
+loglik_comp = pd.concat(loglik_comp, axis=1)
+loglik_comp.columns = pd.Index(subjects, name='subject')
+
+
+#%% log-likelihoods of posterior samples
+fun = lambda x: x.stack().reset_index('subject')
+logliks = pd.concat(
+        [fun(loglik), fun(loglik_comp)], axis=0, 
+        keys=['selected', 'comparison'], 
+        names=['result', 'sample']).reset_index('result')
+
+logliks.columns = ['result', 'subject', 'loglik']
+
+sns.catplot(x="subject", y='loglik', hue="result",
+            kind="box", data=logliks);
+
+fig, ax = plt.subplots()
+sns.distplot(loglik.median() - loglik_comp.median(), rug=True, ax=ax)
+ax.set_xlabel('median loglik difference (new - old)')
+
+
+#%% estimate goodness of fit directly on summary statistics
+within_deviance = lambda x: np.sum(x**2, axis=1)
+within_deviance = lambda x: np.median(np.abs(x), axis=1)
+
+across_deviance = lambda x: np.mean(np.abs(x), axis=0)
+
+fit, eh = snlsim.estimate_posterior_fit(
+        result, subjects, stats, within_deviance, across_deviance)
+
+fig, ax = plt.subplots()
+sns.distplot(fit, rug=True, ax=ax, label='selected')
+
+if comparison_result:
+    fit_comp, eh_comp = snlsim.estimate_posterior_fit(
+            comparison_result, subjects, stats, within_deviance, 
+            across_deviance)
+    sns.distplot(fit_comp, rug=True, ax=ax, label='comparison')
+
+ax.legend()
+
+
+#%% check fit of differences in accuracy and median RT across easy/hard conditions
+fig, axes = plt.subplots(1, 2)
+
+for measure, ax in zip(['accuracy', 'median RT'], axes):
+    sns.distplot(
+            (diff_data[measure] 
+            - diff_post[measure].groupby('subject').median()).dropna(),
+            label='selected', ax=ax, bins=12)
+    sns.distplot(
+            (diff_data[measure] 
+            - diff_post_comp[measure].groupby('subject').median()).dropna(), 
+            label='comparison', ax=ax, bins=12)
+    
+    ax.set_xlabel(measure)
+    ax.legend()
 
 
 #%% 
